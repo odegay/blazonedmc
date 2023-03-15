ae004d84bdf7d8731173624648559ea64bba3afb
dolegan@gmail.com <dolegan@gmail.com>
2023-03-15 09:54:53 -0400

    Geyser updates

--- scripts/blz-start/paper-start.sh
+++ scripts/blz-start/paper-start.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
-source paper-start.cfg
+source ./scripts/blz-start/paper-start.cfg
 if [ $delete_world -eq 1 ]
 then
     echo "delete_world = " $delete_world ", link = " $new_world_link
 fi
 
-echo "done"
+echo "executing paper.jar"
+java -Xms2G -Xmx4G -jar paper.jar --nogui
+
